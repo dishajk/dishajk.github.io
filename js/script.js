80618ad114d7a634c6f3b0946235abf0b7bf04af
dishajk <djkuzhively@gmail.com>
2022-04-21 23:28:36 +0530

    made toggler collapse when out of focus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+let buttontotop = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -11,15 +11,30 @@ function scrollFunction() {
     document.body.scrollTop > 20 ||
     document.documentElement.scrollTop > 20
   ) {
-    mybutton.style.display = "block";
+    buttontotop.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    buttontotop.style.display = "none";
   }
 }
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+buttontotop.addEventListener("click", backToTop);
 
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//collapse toggler
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $(".navbar-toggler").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#navbar-main").collapse('hide');
+    }
+  });
+
+  $(".navbar-toggler").click(function (event) {
+    $(event.target).focus();
+  });
+});
